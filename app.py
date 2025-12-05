84041f6002f194879bad4ed3097897aac3ecd9c9
PussyJussy <1@.com>
2025-12-05 21:39:22 +0300

    New branch for devision, and test for div

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ def greet(name: str):
     return f"Hello, {name}!"
 
 def add(a:int, b:int):
-    return a-b
+    return a+b
 
 def mult(a:int, b:int):
     return a*b
+
+def div(a:int, b,int):
+    return a/b
