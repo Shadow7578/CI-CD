883649875b6d15f784cc4c917b8cbcae49ad2073
PussyJussy <1@.com>
2025-12-08 20:30:50 +0300

    main

--- app.py
+++ app.py
@@ -9,3 +9,8 @@ def mult(a:int, b:int):
 
 def div(a:int, b:int):
     return a/b
+if __name__ == "__main__":
+    print(greet("Stas"))
+    print("2 + 3 =", add(2, 3))
+    print("4 * 5 =", mult(4, 5))
+    print("10 / 2 =", div(10, 2))
